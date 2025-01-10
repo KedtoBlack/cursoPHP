69698205bb401e60d9661a8dbc1fde943d313c51
KEDTO <kedwintorresoaki@gamail.com>
2025-01-09 22:43:14 -0500

    subida clase 3

---
+++ clase3/index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+sitanxis==> array
+arra(
+clave => valor,
+clave => valor,
+clave => valor,
+clave => valor
+.....
+)
+
+*/
+
+$array = array(
+    "carlos",
+    "victor",
+    "maria",
+    "carla"
+);
+var_dump($array);
