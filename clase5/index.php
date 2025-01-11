5bac8f9b80ef025661b1402f468a354d05407546
KEDTO <kedwintorresoaki@gamail.com>
2025-01-10 20:30:03 -0500

    clase5 del curso php

---
+++ clase5/index.php
@@ -0,0 +1,41 @@
+<?php
+
+//Estructuras de control (estructuras condiciones)
+
+//estrucrtura condicional simple
+/*if(condicion){
+    insttrucciones;
+}*/
+
+//$nombre = "goku";
+/*if ($nombre == "goku") {
+    echo "eres un super sayayin";
+}*/
+/*
+if (condicion) {
+    instruccion;
+} else {
+    instruccion:
+}*/
+/*
+if($nombre=="yancha"){
+    echo "es un super sayayin";
+}else{
+    echo "no eres un super sayayin";
+}
+*/
+
+$color="rojo";
+
+if($color=="azul"){
+    echo "si el color es ".$color." aprobaste";
+}elseif($color=="rojo"){
+    echo "si el color es ".$color." desaprobaste";
+}elseif($color=="amarillo"){
+    echo "si el color es ".$color." complementacion";
+}
+
+
+
+
+?>
