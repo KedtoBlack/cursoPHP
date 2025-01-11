e66039816b161cdca890ba98c1faa2d8e7255541
KEDTO <kedwintorresoaki@gamail.com>
2025-01-10 19:36:41 -0500

    operadores clase 4

---
+++ clase4/index.php
@@ -0,0 +1,55 @@
+<?php 
+
+/*operadores
+
+aritmeticos
+
+comparación
+
+logicos
+*/
+//operadores aritmeticos
+$a=3;
+$b=2;
+
+$suma= $b ** $a;
+ 
+//echo $a.' expo '.$b.'= '.$suma;
+
+//operadores de comparacion
+
+$nro_per=6;
+$nro_ani=6;
+/*
+if($nro_per==$nro_ani){
+echo 'pets lover';
+}else{
+    echo "Algunas personas no les gustan los animales";
+}
+*/
+/*
+
+if($nro_per!=$nro_ani){
+    echo 'no hay muchas personas o animales';
+    }else{
+        echo "pets lovers";
+    }*/
+    $h=15;//hjombres
+    $m=26;//mujeres
+/*
+    if($m < $h){
+        echo "somos el matriarcado";
+        
+        }else{
+            echo 'somos el patriarcado';
+        }
+ */
+$profesion="profesor";
+
+   if(($profesion)=="profesor"){
+    echo  "es un docente";
+   } else{
+    echo " tiene otra profesion";
+   }
+
+?>
